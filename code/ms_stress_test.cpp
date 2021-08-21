6686286004872f973ce2bd5ea0903558a38d86c3
AbdeltwabMF <abdeltwab.m.fakhry@gmail.com>
2021-08-21 16:18:29 +0200

    Remove unnecessary sections

--- code/ms_stress_test.cpp
+++ code/ms_stress_test.cpp
@@ -1,26 +1,3 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-#define endl '\n'
-
-using i128 = __int128_t;
-using i64 = int64_t;
-using i32 = int32_t;
-using i16 = int16_t;
-using i8 = int8_t;
-
-using u128 = __uint128_t;
-using u64 = uint64_t;
-using u32 = uint32_t;
-using u16 = uint16_t;
-using u8 = uint8_t;
-
-void fast() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-}
-
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 /** 64-bit signed int Generator
@@ -39,7 +16,7 @@ void gen () {
   while (t--) {
     i32 n = int64(1, 100), m = int64(1, 100);
     cout << n << " " << m << endl;
-    
+
     while (m--) {
       i32 u = int64(1, n), v = int64(1, n), c = int64(1, 4);
       cout << u << " " << v << " " << c << endl;
@@ -49,16 +26,16 @@ void gen () {
 
 i32 main (i32 arg, char* args[]) {
   fast();
-  
+
   i32 tc = 0;
   i32 limit = 100;
   if(arg != 3) return 0;
-  
-  string flags = "g++ -Wall -Wextra -Wshadow -Og -g -Ofast -std=c++17 -D_GLIBCXX_ASSERTIONS -DDEBUG -ggdb3 -fsanitize=address,undefined -fmax-errors=2 -o "; 
+
+  string flags = "g++ -Wall -Wextra -Wshadow -Og -g -Ofast -std=c++17 -D_GLIBCXX_ASSERTIONS -DDEBUG -ggdb3 -fsanitize=address,undefined -fmax-errors=2 -o ";
   string ex = ".cpp", bf, oz, pr;
 
-  bf = flags + args[1] + " " + args[1] + ex; 
-  oz = flags + args[2] + " " + args[2] + ex; 
+  bf = flags + args[1] + " " + args[1] + ex;
+  oz = flags + args[2] + " " + args[2] + ex;
   char bff[bf.size() + 1];
   char ozz[oz.size() + 1];
   strcpy(bff, bf.c_str());
