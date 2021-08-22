27b5afe43d6a697cc004039d571cd77403daa5eb
AbdeltwabMF <abdeltwab.m.fakhry@gmail.com>
2021-08-22 14:46:17 +0200

    Refactor dirs

--- code/ms_PRNG.cpp
+++
@@ -1,7 +0,0 @@
-/** pseudo-random number generator | C++xx >= C++11 **/
-
-mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-
-T myRand(T a, T b) {
-  return uniform_int_distribution <T> (a, b)(rng);
-}
